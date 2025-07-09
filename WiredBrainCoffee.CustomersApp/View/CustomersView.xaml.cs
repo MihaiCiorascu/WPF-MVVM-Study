441831564b6861a1f10a8d6d6fe8e4cf08303840
Mihai Ciorasu <mihai.ciorascu@napa.fi>
2025-07-09 13:45:53 +0300

    Initial commit

---
+++ WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
+
+namespace WiredBrainCoffee.CustomersApp.View
+{
+  public partial class CustomersView : UserControl
+  {
+    private CustomersViewModel viewModel;
+    
+    public CustomersView()
+    {
+      InitializeComponent();
+      viewModel = new CustomersViewModel(new CustomerDataProvider());
+      DataContext = viewModel;
+      Loaded += CustomersView_Loaded;
+    }
+
+    private async void CustomersView_Loaded(object sender, RoutedEventArgs e)
+    {
+        await viewModel.LoadAsync();
+    }
+  }
+}
---
+++ WiredBrainCoffee.CustomersApp/ViewModel/CustomerItemViewModel.cs
@@ -0,0 +1,47 @@
+﻿using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class CustomerItemViewModel : ViewModelBase
+    {
+        private readonly Customer model;
+
+        public CustomerItemViewModel(Customer model) 
+        {
+            this.model = model;
+        }
+
+        public int Id => this.model.Id;
+
+        
+        public string? FirstName
+        {
+            get => this.model.FirstName;
+            set
+            {
+                this.model.FirstName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string? LastName
+        {
+            get => this.model.LastName;
+            set
+            {
+                this.model.LastName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool IsDeveloper
+        {
+            get => this.model.IsDeveloper;
+            set
+            {
+                this.model.IsDeveloper = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
